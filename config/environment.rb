dac3810a3e83f2fcfd8dfe817a5b9738b0f21820
Roman Heinrich <roman.heinrich@gmail.com>
2009-07-26 16:57:12 +0200

    changed readme + environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
+require 'esi_for_rack'
 
 Rails::Initializer.run do |config|
   config.plugins = [ :all ]
   config.time_zone = 'UTC'
-  config.middleware.use  "EsiForRack", {}
+  config.middleware.use  EsiForRack
 end
